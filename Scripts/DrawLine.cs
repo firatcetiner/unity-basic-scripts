45b2b4b154052c183d12710c09f83086cfa54606
Fırat Çetiner <f1r4tc@gmail.com>
2019-02-08 17:38:41 +0300

    Adding some comments

--- Scripts/DrawLine.cs
+++ Scripts/DrawLine.cs
@@ -26,7 +26,9 @@ namespace Utils
 
 		private void Update()
 		{
+			// keep tracking the latest mouse position - which is line's 2nd position - and move 1st position to 2nd
 			line.SetPosition(0, Vector3.MoveTowards(line.GetPosition(0), line.GetPosition(1), Time.deltaTime * lineSpeed));
+			/* If mouse button is pressed, remove the points in the list so that the line will be invisible */
 			if (Input.GetMouseButtonDown(0))
 			{
 				isMousePressed = true;
@@ -42,7 +44,7 @@ namespace Utils
 			mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
 			mousePos.z = 0;
 			
-			
+			/* Adding mouse position(Vector3) to point list, then draw line */
 			for (var i = 1; i < 3; i++)
 			{
 				if (pointList.Count >= 2) continue;
@@ -50,13 +52,15 @@ namespace Utils
 				line.positionCount = pointList.Count;
 				line.SetPosition(pointList.Count - 1, pointList[pointList.Count - 1]);
 			}
+			// Set line's 2nd position to be the mouse position.
 			line.SetPosition(1, mousePos);
 			
 		}
-
+		
+		/* Initialize method for LineRenderer */
 		private void CreateAndSetLine()
 		{		
-			line = gameObject.AddComponent<LineRenderer>();
+			line = gameObject.AddComponent<LineRenderer>(); // Add a new LineRenderer component to gameObject
 			
 			/* no shadow things */
 			line.receiveShadows = false;
@@ -64,18 +68,20 @@ namespace Utils
 			line.allowOcclusionWhenDynamic = false;
 			line.shadowBias = 0.0f;
 			
-			/* create and set material */
+			/* Create and set material */
 			line.material =  new Material(Shader.Find("Particles/Standard Unlit"));
 			
-			/* other line settings */
+			/* Other line settings */
 			line.positionCount = 0;
+			/* Width settings for the line,
+			 * you can adjust these values to make the line more spesific */
 			line.startWidth = 0.2f;
 			line.endWidth = 0.2f;
+			
 			line.startColor = lineColor;
 			line.endColor = lineColor;
 			line.useWorldSpace = true;
-			line.numCapVertices = 30; // make it less edgy
+			line.numCapVertices = 30; // Make the line less edgy
 		}
     }	     
 }
-
